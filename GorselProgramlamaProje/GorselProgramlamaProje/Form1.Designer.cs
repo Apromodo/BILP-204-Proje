d81dea82b2429dfcae6b2c0d6797ca3546339053
Kaya  Ünal <kayaunal92@gmail.com>
2020-04-20 18:25:26 +0300

    v1.0

---
+++ GorselProgramlamaProje/GorselProgramlamaProje/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace GorselProgramlamaProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.Subject = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Body = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DateTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnReceive = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(1800, 438);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(181, 47);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Gönder!";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kime:";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(197, 92);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(1784, 31);
+            this.txtTo.TabIndex = 2;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(197, 141);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(1784, 31);
+            this.txtSubject.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Başlık:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(197, 199);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMessage.Size = new System.Drawing.Size(1784, 217);
+            this.txtMessage.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Messaj:";
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(975, 501);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1006, 721);
+            this.webBrowser.TabIndex = 7;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Subject,
+            this.Body,
+            this.Sender,
+            this.DateTime});
+            this.dataGrid.Location = new System.Drawing.Point(82, 501);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowHeadersWidth = 82;
+            this.dataGrid.RowTemplate.Height = 33;
+            this.dataGrid.Size = new System.Drawing.Size(871, 721);
+            this.dataGrid.TabIndex = 8;
+            this.dataGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGrid_CellClick);
+            // 
+            // Subject
+            // 
+            this.Subject.DataPropertyName = "Subject";
+            this.Subject.HeaderText = "Subject";
+            this.Subject.MinimumWidth = 10;
+            this.Subject.Name = "Subject";
+            this.Subject.Width = 200;
+            // 
+            // Body
+            // 
+            this.Body.DataPropertyName = "Body";
+            this.Body.HeaderText = "Body";
+            this.Body.MinimumWidth = 10;
+            this.Body.Name = "Body";
+            this.Body.Visible = false;
+            this.Body.Width = 200;
+            // 
+            // Sender
+            // 
+            this.Sender.DataPropertyName = "Sender";
+            this.Sender.HeaderText = "Sender";
+            this.Sender.MinimumWidth = 10;
+            this.Sender.Name = "Sender";
+            this.Sender.Width = 200;
+            // 
+            // DateTime
+            // 
+            this.DateTime.DataPropertyName = " Date";
+            this.DateTime.HeaderText = "DateTime";
+            this.DateTime.MinimumWidth = 10;
+            this.DateTime.Name = "DateTime";
+            this.DateTime.Width = 200;
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(1710, 1255);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(271, 73);
+            this.btnReceive.TabIndex = 9;
+            this.btnReceive.Text = "İletileri al!";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2068, 1360);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Subject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Body;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DateTime;
+    }
+}
+
---
+++ GorselProgramlamaProje/GorselProgramlamaProje/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mailServisi = Microsoft.Office.Interop.Outlook;
+
+namespace GorselProgramlamaProje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+//------------------------------------Mail Gönderme İşlemı try carh ile durum bilidirimi------------------------------
+
+
+            try
+            {
+                mailServisi._Application _app = new mailServisi.Application();
+                mailServisi.MailItem mail = (mailServisi.MailItem)_app.CreateItem(mailServisi.OlItemType.olMailItem);
+                mail.To = txtTo.Text;
+                mail.Subject = txtSubject.Text;
+                mail.Body = txtMessage.Text;
+                mail.Importance = mailServisi.OlImportance.olImportanceNormal;
+                ((mailServisi.MailItem)mail).Send();
+                MessageBox.Show("MESSAGJE BAŞARILI", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Gönderme Başarısız!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+
+
+        //-------------------------------------------------------Gelen Mesajları İndirme-----------------------------------
+
+        DataTable dt;
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+            try
+            {
+
+                mailServisi._Application _app = new mailServisi.Application();
+                mailServisi._NameSpace _ns = _app.GetNamespace("MAPI");
+                mailServisi.MAPIFolder Inbox = _ns.GetDefaultFolder(mailServisi.OlDefaultFolders.olFolderInbox);
+                _ns.SendAndReceive(true);
+                dt = new DataTable("Inbox");
+                dt.Columns.Add("Subject", typeof(String));
+                dt.Columns.Add("Sender", typeof(String));
+                dt.Columns.Add("Body", typeof(String));
+                dt.Columns.Add("Date", typeof(String));
+                dataGrid.DataSource = dt;
+                foreach (mailServisi.MailItem item in Inbox.Items)
+                    dt.Rows.Add(new object[] { item.Subject, item.SenderName, item.HTMLBody, item.SentOn.ToLongDateString() + " " + item.SentOn.ToLongTimeString() });
+            }
+
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "İndirme Başarısız!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+
+
+        }
+
+
+        //----------------------------------------Düzgün sıra ile gösterme ama 3 taneden fazla olunca indirmiyor
+
+
+
+
+        private void dataGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < dt.Rows.Count && e.RowIndex >= 0)
+                webBrowser.DocumentText = dt.Rows[e.RowIndex]["Body"].ToString();
+        }
+    }
+}
